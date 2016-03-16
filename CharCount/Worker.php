28e666746b43c726ccf43321753b586669962bc0
Diana Ungaro Arnos <hmdiana@gmail.com>
2016-03-16 08:16:08 -0300

    Add worker, update gitignore

--- CharCount/Worker.php
+++ CharCount/Worker.php
@@ -9,5 +9,29 @@ namespace DianaArnos\DarkmiraTourBR2016\CharCount;
 
 class Worker
 {
+    private $socket;
 
+    public function __construct($argv)
+    {
+        $this->socket = $argv[1];
+    }
+
+    public function run()
+    {
+        $server = stream_socket_server("tcp://$this->socket", $errorNum, $errorMsg);
+
+        if ($server === false) {
+            throw new \UnexpectedValueException(
+                "Não foi possível a conexão com o socket: {$errorNum} - {$errorMsg}" . PHP_EOL
+            );
+        }
+
+        while ($conn = stream_socket_accept($server)) {
+            $result = strlen(base64_decode(fread($conn, 13421772)));
+            fwrite($conn, $result . PHP_EOL);
+        }
+    }
 }
+
+$worker = new Worker($argv);
+$worker->run();
